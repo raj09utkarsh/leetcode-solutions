5229f81440e611def604caa1a22f667daaf144ee
Utkarsh Raj <raj09utkarsh@gmail.com>
2019-12-22 00:49:02 +0530

    add minimum-path-sum solution

---
+++ minimum-path-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(n == 0){
+            return 0;
+        }
+        int m = grid[0].size();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i-1 >= 0 && j-1 >= 0){
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+                }
+                else if(i-1 >= 0){
+                    grid[i][j] += grid[i-1][j];
+                }
+                else if(j-1 >= 0){
+                    grid[i][j] += grid[i][j-1];
+                }
+            }
+        }
+        
+        return grid[n-1][m-1];
+    }
+};
